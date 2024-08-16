d04bc46a16cc68d3c54d4035a9e6669662c29a9f
Clifford Loo <clifford.loo@gmail.com>
2024-08-15 23:25:14 -0700

    debug7

--- full-test.sh
+++ full-test.sh
@@ -25,7 +25,7 @@ set +e
 if [ -f conf/assignment.txt ]; then
     echo "Found conf/assignment.txt"
     ls conf
-    ls assignment-autotest/test
+    ls .
     # This is just one example of how you could find an associated assignment
     assignment=`cat conf/assignment.txt`
     if [ -f ./assignment-autotest/test/${assignment}/assignment-test.sh ]; then
